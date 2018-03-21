fc20802e864f3954205d9ca80c5b1d3f773daea1
Taylor MacDonald <tcmacdonald@gmail.com>
2018-03-21 15:01:05 -0400

    Dynamic post creation, proof of concept.

---
+++ _plugins/hubspot_generator.rb
@@ -0,0 +1,26 @@
+require 'pry'
+require 'jekyll'
+
+module Jekyll
+
+  class HubspotPost < Document
+    def draft?
+      false
+    end
+  end
+
+  class HubspotGenerator < Generator
+    def generate(site)
+      doc = HubspotPost.new('_posts/2018-03-21-something.md', site: site, collection: site.posts)
+
+      doc.data['title'] = 'This is a test'
+      doc.data['layout'] = 'blog'
+      doc.data['date'] = Time.now
+      doc.data['slug'] = 'this-is-a-test'
+      doc.content = 'Curabitur blandit tempus porttitor.'
+
+      site.posts.docs << doc
+    end
+  end
+
+end
