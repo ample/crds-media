988d772006d971e4be6e18955782a6549c46a64e
Taylor MacDonald <tcmacdonald@gmail.com>
2018-04-25 16:19:47 -0400

    US12391 AWS Cloudsearch Change order

--- _migrations/01-create-authors.js
+++ _migrations/01-create-authors.js
@@ -3,8 +3,8 @@ module.exports = function (migration) {
     .name('Author')
     .description('Person associated with one or more pieces of content');
 
-  author.createField('slug').name('Slug').type('Symbol').required(true).validations([{ unique: true }]);
   author.createField('full_name').name('Full Name').type('Symbol').required(true);
+  author.createField('slug').name('Slug').type('Symbol').required(true).validations([{ unique: true }]);
   author.createField('summary').name('Summary').type('Text').required(true);
   author.createField('body').name('Bio').type('Text');
   author.createField('image').name('Image').type('Link').linkType('Asset').required(true);
