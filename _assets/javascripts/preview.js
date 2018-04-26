35c80fc4e57be24cdecd5c6130a4a4cf46e4cefb
Luke Moderwell <luke.moderwell@gmail.com>
2018-04-26 14:46:09 -0400

    preview WIP

---
+++ _assets/javascripts/preview.js
@@ -0,0 +1,63 @@
+// import markdown from 'markdown';
+
+// dom stuff
+var title = document.querySelector('[data-title]');
+var authorLink = document.querySelector('[data-author-link]');
+var postDate = document.querySelector('[data-post-date]');
+var postBody = document.querySelector('[data-body]');
+var tagContainer = document.querySelector('[data-tags]');
+
+function getUrlParameter() {
+  var sPageURL = decodeURIComponent(window.location.search),
+  sParameterName = sPageURL.split('=');
+  return sParameterName[1];
+}
+
+function createTags(arr) {
+  for (var i = 0; i < arr.length; i+= 1) {
+    var elm = document.createElement('a');
+    elm.classList.add('article_tag');
+    elm.innerText = arr[i];
+    tagContainer.appendChild(elm);
+  }
+}
+
+function setImageSrc(obj) {
+
+}
+
+function setText(obj) {
+  title.innerText = obj.title;
+  postDate.innerText = obj.published_at;
+  authorLink.innerText = obj.author.sys.id;
+  postBody.innerText = obj.body;
+}
+
+function renderPage(data) {
+  setText(data);
+  createTags(data.tags);
+}
+
+function reqListener () {
+  var res = JSON.parse(this.response);
+  let data = [];
+  console.log(res)
+  for (let i = 0; i < res.items.length; i += 1) {
+    var postId = getUrlParameter();
+    if (res.items[i].sys.id == postId) {
+      data.push(res.items[i].fields);
+    }
+  }
+  console.log(data[0]);
+  renderPage(data[0]);
+}
+
+// make a request to Contentful for preview content
+function fetchData(url) {
+  var newXHR = new XMLHttpRequest();
+  newXHR.open( 'GET', url );
+  newXHR.send();
+  newXHR.addEventListener( 'load', reqListener );
+}
+
+fetchData('https://preview.contentful.com/spaces/y3a9myzsdjan/environments/master/entries?access_token=31349caf7c9b1e390583a84a741a754d976984c27c2c70d831050b9804e80edf');
