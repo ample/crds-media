fa71751c3c7b468638a04a91cc3ab62f01a19e82
Sean C Davis <scdavis41@gmail.com>
2018-05-03 13:35:43 -0400

    US13259 - Audit Assets Write purgecss config to (tmp) file before running command

--- _plugins/purgecss.rb
+++ _plugins/purgecss.rb
@@ -1,8 +1,23 @@
 Jekyll::Hooks.register(:site, :after_init) do |_site|
+  # Before we begin building, delete any lingering CSS files. This ensures we
+  # don't purge the incorrect file.
   Dir.glob('_site/assets/*.css').each { |f| File.delete(f) }
 end
 
 Jekyll::Hooks.register(:site, :post_write) do |_site|
-  css_file = Dir.glob('_site/assets/*.css').first
-  system("purgecss --css #{css_file} --out _site/assets --content _site/**/*.html")
+  # Temp file to store options. Command line would not accept a series of
+  # whitelist classes, and there are a few classes purgecss is missing.
+  config_file = '_plugins/purgecss.js'
+  # Configuration JS to write to the file. (Docs: https://www.purgecss.com/configuration)
+  config_text = """module.exports = #{{
+    content: ['_site/**/*.html'],
+    css: [Dir.glob('_site/assets/*.css').first],
+    whitelist: ['pull-right', 'border-bottom']
+  }.stringify_keys.to_json}"""
+  # Write configuration file.
+  File.open(config_file, 'w+') { |f| f.write(config_text) }
+  # Run purgecss command.
+  system("purgecss --config #{config_file} --out _site/assets")
+  # Delete configuration file.
+  File.delete(config_file)
 end
