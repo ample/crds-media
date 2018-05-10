60faf1c4ade23ec7f66247d374a8f273edda61a9
Sean C Davis <scdavis41@gmail.com>
2018-05-10 15:47:57 -0400

    US13435 - Podcast Content Model Update episode content model and clean up some styles

--- _migrations/04-create-podcasts.js
+++ _migrations/04-create-podcasts.js
@@ -1,23 +1,23 @@
 module.exports = function (migration) {
-  const article = migration.createContentType('podcast')
+  const podcast = migration.createContentType('podcast')
     .name('Podcast')
     .description('Podcast, or a grouping of audio episodes')
     .displayField('title');
 
-  article.createField('title').name('Title').type('Symbol').required(true);
-  article.createField('subtitle').name('Subtitle').type('Symbol');
-  article.createField('slug').name('Slug').type('Symbol').required(true).validations([{ unique: true }]);
-  article.createField('description').name('Description').type('Text');
-  article.createField('image').name('Image').type('Link').linkType('Asset');
-  article.createField('bg_image').name('Background Image').type('Link').linkType('Asset');
-  article.createField('author').name('Author').type('Array').items({
+  podcast.createField('title').name('Title').type('Symbol').required(true);
+  podcast.createField('subtitle').name('Subtitle').type('Symbol');
+  podcast.createField('slug').name('Slug').type('Symbol').required(true).validations([{ unique: true }]);
+  podcast.createField('description').name('Description').type('Text');
+  podcast.createField('image').name('Image').type('Link').linkType('Asset');
+  podcast.createField('bg_image').name('Background Image').type('Link').linkType('Asset');
+  podcast.createField('author').name('Author').type('Array').items({
     type: 'Link',
     linkType: 'Entry',
     validations: [{ linkContentType: [ 'author' ] }]
   });
-  article.createField('apple_podcasts_url').name('Apple Podcasts URL').type('Symbol');
-  article.createField('google_play_url').name('Google Play URL').type('Symbol');
-  article.createField('stitcher_url').name('Stitcher URL').type('Symbol');
+  podcast.createField('apple_podcasts_url').name('Apple Podcasts URL').type('Symbol');
+  podcast.createField('google_play_url').name('Google Play URL').type('Symbol');
+  podcast.createField('stitcher_url').name('Stitcher URL').type('Symbol');
 
-  article.changeEditorInterface('slug', 'slugEditor');
+  podcast.changeEditorInterface('slug', 'slugEditor');
 };
