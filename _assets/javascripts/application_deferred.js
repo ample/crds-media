d4d0819932faefcf5ac95f4b075c6b9a53dd3b2c
LeeAnne Galioto <leeanne@helloample.com>
2018-05-10 16:49:43 -0400

    Include Card Carousels

--- _assets/javascripts/application_deferred.js
+++ _assets/javascripts/application_deferred.js
@@ -3,7 +3,9 @@
 //= require ./vendor/imgix.min
 //= require ./vendor/addthis_widget.min
 //= require ./vendor/clipboard.min
+//= require ./vendor/crds-card-carousel-v0.2.0.min
 //= require ./components/clipboard
 //= require ./components/gtm
 //= require ./components/header
 //= require ./components/images
+//= require ./components/new-card-carousel
