60ed47dca9015689b8b767f48a519180a7293441
Taylor MacDonald <tcmacdonald@gmail.com>
2018-05-10 22:01:54 -0400

    US13422 Podcasts Explore More Implement card carousels

--- _assets/javascripts/application_deferred.js
+++ _assets/javascripts/application_deferred.js
@@ -3,7 +3,7 @@
 //= require ./vendor/addthis_widget.min
 //= require ./vendor/clipboard.min
 //= require ./vendor/crds-card-carousel-v0.2.0.min
-//= require ./vendor/crds-shared-header-v0.7.1.min
+//= require ./vendor/flickity.pkgd.min
 //= require ./components/clipboard
 //= require ./components/gtm
 //= require ./components/header
