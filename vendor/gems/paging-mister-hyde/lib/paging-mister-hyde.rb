0eb371a5ce5e3ab7414f83ec4011ace261852b00
Taylor MacDonald <tcmacdonald@gmail.com>
2018-05-16 17:10:38 -0400

    US13338 Pagination Proof of concept

--- vendor/gems/paging-mister-hyde/lib/paging-mister-hyde.rb
+++ vendor/gems/paging-mister-hyde/lib/paging-mister-hyde.rb
@@ -5,6 +5,24 @@ class PagingMisterHyde < Jekyll::Generator
   attr_accessor :site
 
   def generate(site)
-    binding.pry
+    site.pages.
+      select{|page| page.data.dig('paginate') }.
+      map do |manifest|
+        cfg = manifest.data.dig('paginate')
+        cfg.keys.map do |content_type|
+          collections = site.collections[content_type].docs.each_slice(cfg.dig(content_type, 'per')).to_a
+          manifest.data[content_type] = collections.first
+
+          collections[1..collections.count].each_with_index do |docs, i|
+            basename = File.basename(manifest.instance_variable_get('@path'), '.*')
+            page = Jekyll::Page.new(site, manifest.instance_variable_get('@base'), manifest.instance_variable_get('@dir'), File.basename(manifest.instance_variable_get('@path')))
+            page.instance_variable_set('@url', "/#{basename}/page/#{i+2}/index.html")
+            page.data[content_type] = docs
+
+            # binding.pry
+            site.pages << page
+          end
+        end
+      end
   end
 end
