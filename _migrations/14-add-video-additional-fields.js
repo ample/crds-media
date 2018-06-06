408a638bfe298661a8a8ebee22b7445f2122a41b
LeeAnne Galioto <leeanne@helloample.com>
2018-06-06 12:27:56 -0400

    US13322 - Video detail page

--- _migrations/14-add-video-additional-fields.js
+++ _migrations/14-add-video-additional-fields.js
@@ -7,20 +7,3 @@ module.exports = function (migration) {
   video.createField('google_play_url').name('Google Play URL').type('Symbol');
 
 };
-
-// DELETE BELOW
-// Already created:
-video.createField('title').name('Title').type('Symbol').required(true);
-video.createField('subtitle').name('Subtitle').type('Symbol');
-video.createField('slug').name('Slug').type('Symbol').required(true).validations([{ unique: true }]);
-video.createField('description').name('Description').type('Text');
-video.createField('author').name('Author').type('Array').items({
-  type: 'Link',
-  linkType: 'Entry',
-  validations: [{ linkContentType: [ 'author' ] }]
-});
-video.createField('image').name('Image').type('Link').linkType('Asset');
-video.createField('source_link').name('Source URL').type('Symbol');
-video.createField('published_at').name('Published At').type('Date').required(true);
-
-video.changeEditorInterface('slug', 'slugEditor');
