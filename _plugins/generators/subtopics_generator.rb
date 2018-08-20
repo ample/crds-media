fc333b79371ca134b0d92bd3a2acc84043100e3b
Sean C Davis <scdavis41@gmail.com>
2018-08-20 12:49:35 -0400

    US14578 - Topic Detail Logic Cleanup and specs for featured tags

--- _plugins/generators/subtopics_generator.rb
+++ _plugins/generators/subtopics_generator.rb
@@ -2,38 +2,51 @@ module Jekyll
   class SubtopicsGenerator < Jekyll::Generator
 
     def generate(site)
-      # All documents in the Jekyll site.
+      # Global references to docs within Jekyll.
       all_docs = site.collections.values.collect(&:docs).flatten
+      all_tags = site.collections['tags'].docs
+      all_topics = site.collections['topics'].docs
       # Loop through each topic doc.
-      site.collections['topics'].docs.each do |topic|
+      all_topics.each do |topic|
         # Get all docs that have applied this topic.
-        docs = all_docs.select { |d| d.data['topic'] == topic.data['title'] }
-        # Collect any docs that have a date field (mapped through
-        # jekyll-contentful), sort by that field, and apply them to the topic.
-        topic_media = docs
-          .select { |d| d.data['date'] }.sort_by { |d| d.data['date'] }.reverse
+        topic_docs = all_docs.select { |d| d.data['topic'] == topic.data['title'] }
+        # Create a reference to publishable docs within this topic. Docs that
+        # have a `date` field (mapped through jekyll-contentful) are considered
+        # publishable.
+        topic_media = topic_docs.select { |d| d.data['date'] }
+        # Publishable docs should be sorted in reverse chronological order.
+        topic_media.sort_by! { |d| d.data['date'] }.reverse!
+        # Set the featured_media on the topic to the most recent three
+        # publishable docs.
         topic.data['featured_media'] = topic_media.first(3)
-        # Use the associated docs to collect all tag names for the topic.
-        tag_names = docs.collect { |d| d.data['tags'] }.flatten.uniq
-        # Use the tag names to collect the Jekyll docs representing the
+        # Look through all the topic's associated docs to find all tags used
+        # within this topic.
+        tag_names = topic_docs.collect { |d| d.data['tags'] }.flatten.uniq
+        # Use the topic's tag names to collect the Jekyll docs representing the
         # associated tags.
-        topic.data['tags'] = site.collections['tags'].docs
-          .select { |t| tag_names.include?(t.data['title']) }
-          .sort_by { |t| t.data['title'].downcase }
-        # Pull out the featured tags into their own attribute.
-        # featured_tag_ids = topic.data['featured_tag_ids'] || []
-        # topic.data['featured_tags'] = topic.data['tags']
-        #   .select { |t| featured_tag_ids.include?(t.data['id']) }
-        #   .sort_by { |t| featured_tag_ids.index(t.data['id']) }
-
-        topic.data['featured_tags'] = []
+        topic_tags = all_tags.select { |t| tag_names.include?(t.data['title']) }
+        # Sort tags by (case-insensitive) title.
+        topic_tags.sort_by! { |t| t.data['title'].downcase }
+        # Set the tags on the topic to the sorted list of tags used within the
+        # topic.
+        topic.data['tags'] = topic_tags
+        # Reference to an array of `id` fields of tags featured on the topic.
+        # This comes through as nil if it hasn't been set in Contentful, which
+        # is why we need to fall back to an empty array.
         featured_tag_ids = topic.data['featured_tag_ids'] || []
-        featured_tags = topic.data['tags']
-          .select { |t| featured_tag_ids.include?(t.data['id']) }
-          .sort_by { |t| featured_tag_ids.index(t.data['id']) }
-        featured_tags.each do |tag|
+        # Featured tags are found using the featured_tag_ids array to extract
+        # featured from the topic_tags array. Thus, if a tag is set in
+        # Contentful that doesn't belong in this topic, it is ignored.
+        featured_tags = topic_tags.select { |t| featured_tag_ids.include?(t.data['id']) }
+        # Featured tags are sorted by the order from which they came in from
+        # Contentful.
+        featured_tags.sort_by! { |t| featured_tag_ids.index(t.data['id']) }
+        # Set the topic's featured_tag_data by looping through the featured_tags
+        # array and storing a reference to the tag and to the media which has
+        # applied the tag and this topic.
+        topic.data['featured_tag_data'] = featured_tags.map do |tag|
           media = topic_media.select { |t| t.data['tags'].include?(tag.data['title']) }
-          topic.data['featured_tags'].push({ 'tag' => tag, 'media' => media })
+          { 'tag' => tag, 'media' => media }
         end
       end
     end
