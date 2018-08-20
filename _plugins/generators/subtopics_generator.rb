543588e68cdd52c68369f95413940568fa5e3562
Sean C Davis <scdavis41@gmail.com>
2018-08-20 09:50:55 -0400

    US14578 - Topic Detail Logic Add logic to topic detail page

--- _plugins/generators/subtopics_generator.rb
+++ _plugins/generators/subtopics_generator.rb
@@ -2,14 +2,39 @@ module Jekyll
   class SubtopicsGenerator < Jekyll::Generator
 
     def generate(site)
-      docs = site.collections.values.collect(&:docs).flatten
+      # All documents in the Jekyll site.
+      all_docs = site.collections.values.collect(&:docs).flatten
+      # Loop through each topic doc.
       site.collections['topics'].docs.each do |topic|
-        tag_names = docs
-          .select { |d| d.data['topic'] == topic.data['title'] }
-          .collect { |d| d.data['tags'] }.flatten.uniq
+        # Get all docs that have applied this topic.
+        docs = all_docs.select { |d| d.data['topic'] == topic.data['title'] }
+        # Collect any docs that have a date field (mapped through
+        # jekyll-contentful), sort by that field, and apply them to the topic.
+        topic_media = docs
+          .select { |d| d.data['date'] }.sort_by { |d| d.data['date'] }.reverse
+        topic.data['featured_media'] = topic_media.first(3)
+        # Use the associated docs to collect all tag names for the topic.
+        tag_names = docs.collect { |d| d.data['tags'] }.flatten.uniq
+        # Use the tag names to collect the Jekyll docs representing the
+        # associated tags.
         topic.data['tags'] = site.collections['tags'].docs
           .select { |t| tag_names.include?(t.data['title']) }
           .sort_by { |t| t.data['title'].downcase }
+        # Pull out the featured tags into their own attribute.
+        # featured_tag_ids = topic.data['featured_tag_ids'] || []
+        # topic.data['featured_tags'] = topic.data['tags']
+        #   .select { |t| featured_tag_ids.include?(t.data['id']) }
+        #   .sort_by { |t| featured_tag_ids.index(t.data['id']) }
+
+        topic.data['featured_tags'] = []
+        featured_tag_ids = topic.data['featured_tag_ids'] || []
+        featured_tags = topic.data['tags']
+          .select { |t| featured_tag_ids.include?(t.data['id']) }
+          .sort_by { |t| featured_tag_ids.index(t.data['id']) }
+        featured_tags.each do |tag|
+          media = topic_media.select { |t| t.data['tags'].include?(tag.data['title']) }
+          topic.data['featured_tags'].push({ 'tag' => tag, 'media' => media })
+        end
       end
     end
 
