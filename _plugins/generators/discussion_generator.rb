599558997be8d22dacf00b14e0ce8774244f8def
zarahzachz <sarah.sachs@gmail.com>
2018-08-29 10:49:34 -0400

    US14736 - Chaser markup Refactor to include array of collection types and add documentation.

--- _plugins/generators/discussion_generator.rb
+++ _plugins/generators/discussion_generator.rb
@@ -4,20 +4,18 @@ module Jekyll
     def generate(site)
       # Global references to docs within Jekyll.
       all_docs = site.collections.values.collect(&:docs).flatten
-      all_articles = site.collections['articles'].docs
       all_questions = site.collections['discussion_questions'].docs
-      # Cycle through each of the articles in the sites' articles collection.
-      all_articles.each do |article|
-        # Find discussion questions
-        article_questions = article.data.dig('discussion', 'raw', 'fields', 'discussion_questions') || []
-        # binding.pry
-        article_question_ids = article_questions.collect { |q| q['sys']['id'] }
-        article.data['discussion_questions'] = all_questions.select { |q| article_question_ids.include?(q.data['id']) }
+      %w{articles messages}.each do |collection_name|
+        # Cycle through each collection type.
+        site.collections[collection_name].docs.each do |doc|
+          # Find discussion questions array
+          doc_questions = doc.data.dig('discussion', 'raw', 'fields', 'discussion_questions') || []
+          # Find ID for each discussion question
+          doc_question_ids = doc_questions.collect { |q| q['sys']['id'] }
+          # Match discussion question ID to discussion_questions collection entries.
+          doc.data['discussion_questions'] = all_questions.select { |q| doc_question_ids.include?(q.data['id']) }
+        end
       end
     end
-
-    # Loop through all the discussion questions in each article and pull out the id
-    # Then I need to match that id value to the id present in the discussion_questions collection
-
   end
 end
