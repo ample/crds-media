fcb6b8281b9a95755c5dca688817a0d9c317eb8f
Sean C Davis <scdavis41@gmail.com>
2018-09-26 08:57:37 -0400

    US14979 - Chase Roll Call Move vue project into vendor directory

---
+++ vendor/web-components/crds-roll-call/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <crds-roll-call></crds-roll-call>
+  </div>
+</template>
+
+<script>
+import CrdsRollCall from './components/crds-roll-call.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CrdsRollCall
+  }
+}
+</script>
+
+<style>
+  @import 'https://d1tmclqz61gqwd.cloudfront.net/styles/crds-styles-3.0.5.min.css';
+</style>
---
+++ vendor/web-components/crds-roll-call/src/components/crds-roll-call.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="crds-roll-call">
+    <div class="btn-group-bar">
+      <button class="btn btn-lg btn-cyan btn-icon" v-on:click="decreaseTotal()" type="button">
+        <svg class="icon icon-1" viewBox="0 0 256 256">
+          <use xlink:href="/assets/svgs/icons.svg#minus"></use>
+        </svg>
+      </button>
+      <button class="btn btn-lg btn-cyan btn-icon" v-on:click="increaseTotal()" type="button">
+        <svg class="icon icon-1" viewBox="0 0 256 256">
+          <use xlink:href="/assets/svgs/icons.svg#plus"></use>
+        </svg>
+      </button>
+    </div>
+    <div>
+      {{ total }} {{ labelNoun }} {{ labelVerb }} discussing these questions
+    </div>
+    <div>
+      <button class="btn btn-primary" v-on:click="submitData()">Submit group size</button>
+    </div>
+    <p>
+      This information helps us ... do something?
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CrdsRollCall',
+    data: function() {
+      return {
+        total: 0,
+        labelNoun: 'people',
+        labelVerb: 'are'
+      }
+    },
+    methods: {
+      decreaseTotal: function() {
+        if (this.total > 0) this.total--;
+        this.updateLabel();
+        return;
+      },
+      increaseTotal: function() {
+        this.total++;
+        this.updateLabel();
+        return;
+      },
+      updateLabel: function() {
+        if (this.total == 1) {
+          this.labelNoun = 'person';
+          this.labelVerb = 'is';
+        } else {
+          this.labelNoun = 'people';
+          this.labelVerb = 'are';
+        }
+      },
+      submitData: function() {
+        console.log('SUBMIT DATA ...');
+      }
+    }
+  }
+</script>
