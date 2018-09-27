d982d66c0c5dda5c0d62cfbe075a3a02c9bfee01
Sean C Davis <scdavis41@gmail.com>
2018-09-27 12:15:19 -0400

    US14979 - Chase Roll Call Get track viewport working directly

--- _assets/javascripts/components/track-viewport.js
+++ _assets/javascripts/components/track-viewport.js
@@ -1,3 +1,62 @@
 (function() {
-  console.log('track-viewport');
+  var initTrackable = function(idx, el) {
+    var options = resolveOptions(el);
+    var viewableAt = null;
+    var tracked = false;
+
+    var checkPosition = function() {
+      if (tracked) return;
+      var rect = el.getBoundingClientRect();
+      if (
+        rect.top <= $(window).height() &&
+        rect.left <= $(window).width() &&
+        rect.bottom >= 0 &&
+        rect.right >= 0
+      ) {
+        beginCount();
+      } else {
+        stopCount();
+      }
+    }
+
+    var beginCount = function() {
+      if (!viewableAt) viewableAt = new Date;
+      setTimeout(count, options.duration + 100);
+    }
+
+    var stopCount = function() {
+      if (viewableAt) return viewableAt = null;
+    }
+
+    var count = function() {
+      if (!viewableAt) return;
+      var viewableFor = new Date - viewableAt;
+      if (viewableFor >= options.duration) track();
+    }
+
+    var track = function() {
+      if (tracked) return;
+      // console.log('TRACK');
+      if (typeof analytics !== 'undefined') analytics.track(options.event, {});
+      tracked = true;
+    }
+
+    $(window).on('DOMContentLoaded load resize scroll', checkPosition);
+  }
+
+  var resolveOptions = function(el) {
+    var defaultOptions = {
+      duration: 5000,
+      event: 'TrackViewport'
+    };
+    var options = $(el).data('track-viewport');
+    if (typeof options !== 'object') options = {};
+    if (!options['duration']) options['duration'] = defaultOptions['duration'];
+    if (!options['event']) options['event'] = defaultOptions['event'];
+    return options;
+  }
+
+  $(document).ready(function() {
+    $('[data-track-viewport]').each(initTrackable)
+  });
 })();
