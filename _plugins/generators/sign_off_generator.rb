45e5ee5d1c12ed1ca1342c3256a1ca044d287a0f
Sean C Davis <scdavis41@gmail.com>
2018-10-04 14:21:39 -0400

    US14642 - Update Jekyll Contentful Set sign_off to most recently published entry and add specs

--- _plugins/generators/sign_off_generator.rb
+++ _plugins/generators/sign_off_generator.rb
@@ -2,7 +2,10 @@ module Jekyll
   class SignOffGenerator < Generator
 
     def generate(site)
-      site.config['sign_off'] = site.collections['sign_offs'].docs.first
+      site.config['sign_off'] = site.collections['sign_offs'].docs
+        .select { |d| d.data['published_at'] }
+        .sort_by { |d| d.data['published_at'] }
+        .reverse.first
     end
 
   end
