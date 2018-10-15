88735d35c7c1d8185546108b60f83a9232711e47
Sean C Davis <scdavis41@gmail.com>
2018-10-15 16:44:47 -0400

    Placeholder asset tag

---
+++ _plugins/tags/asset_tag.rb
@@ -0,0 +1,16 @@
+module Jekyll
+  class AssetTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+    end
+
+    def render(context)
+      ''
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('asset', Jekyll::AssetTag)
