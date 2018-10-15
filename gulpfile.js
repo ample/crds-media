093efe16763ab402a8649a83bbf8284a73a60b4a
Sean C Davis <scdavis41@gmail.com>
2018-10-15 14:43:42 -0400

    Add purgecss to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var tildeImporter = require('node-sass-tilde-importer');
+const gulp = require('gulp');
+const run = require('gulp-run-command');
+const sass = require('gulp-sass');
+const tildeImporter = require('node-sass-tilde-importer');
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src('./_assets/stylesheets/application.scss')
     .pipe(sass({
       importer: tildeImporter
     }).on('error', sass.logError))
     .pipe(gulp.dest('./_site/assets'));
 });
+
+gulp.task('purgecss', run('purgecss --config ./purgecss.config.json'))
+
 
 // gulp.task('sass:watch', function () {
 //   gulp.watch('./sass/**/*.scss', ['sass']);
