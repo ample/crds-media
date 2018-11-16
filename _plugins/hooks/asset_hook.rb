28d9830ec608578628f962b8660f3a47e452f388
Sean C Davis <scdavis41@gmail.com>
2018-11-16 17:17:40 -0500

    US15390 - Asset Pipeline Intelligently figure out when to build assets

--- _plugins/hooks/asset_hook.rb
+++ _plugins/hooks/asset_hook.rb
@@ -1,16 +1,42 @@
 require 'securerandom'
+require 'fileutils'
+
+# TODO: Refactor, then document.
 
 Jekyll::Hooks.register(:site, :after_init) do |site|
   site.config['asset_dest'] ||= 'assets'
-  site.config['asset_hash'] = SecureRandom.hex(12)
 
-  binding.pry
-
-  if ((ENV['JEKYLL_ENV'] ||= 'development') == 'development')
+  if (ENV['JEKYLL_ENV'] ||= 'development') == 'development'
     (site.config['keep_files'] ||= []).push(site.config['asset_dest'])
   end
 end
 
+Jekyll::Hooks.register(:site, :pre_render) do |site|
+  asset_hash_file = 'tmp/.asset_hash'
+  asset_dir = "#{site.config['destination']}/#{site.config['asset_dest']}"
+  asset_src_dir = '_assets'
+
+  asset_files = Dir.glob("#{asset_dir}/**/*").select { |f| f.ends_with?('.css') || f.ends_with?('.js') }
+  asset_src_files = Dir.glob("#{asset_src_dir}/**/*").select { |f| f.ends_with?('.scss') || f.ends_with?('.js') }
+
+  site.config['build_assets'] = false
+
+  if ENV['BUILD_ASSETS'].to_s == 'true' || !File.exists?(asset_hash_file) || asset_files.blank? || asset_src_files.select { |f| File.mtime(f) >= File.mtime(asset_hash_file) }.any?
+    site.config['asset_hash'] = SecureRandom.hex(12)
+    site.config['build_assets'] = true
+  else
+    site.config['asset_hash'] = File.read(asset_hash_file)
+  end
+end
+
 Jekyll::Hooks.register(:site, :post_write) do |site|
-  system("ASSET_HASH=#{site.config['asset_hash']} npm run build")
+  asset_hash_file = 'tmp/.asset_hash'
+  asset_dir = "#{site.config['destination']}/#{site.config['asset_dest']}"
+
+  if site.config['build_assets']
+    %w{js css}.each { |ext| FileUtils.rm(Dir.glob("#{asset_dir}/**/*.#{ext}")) }
+
+    system("ASSET_HASH=#{site.config['asset_hash']} npm run build")
+    File.open(asset_hash_file, 'w+') { |f| f.write(site.config['asset_hash']) }
+  end
 end
