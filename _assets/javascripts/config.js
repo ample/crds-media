366558550aa9d806ae4c715257e56790afe373b6
zarahzachz <sarah.sachs@gmail.com>
2018-12-07 11:48:46 -0500

    US14301 - YT click events Refactor in Gucci.

--- _assets/javascripts/config.js
+++ _assets/javascripts/config.js
@@ -35,6 +35,7 @@ module.exports = [
       'components/dates',
       'components/menu-squencher',
       'components/status-message',
+      'components/yt-event-handler',
       'lib/header'
     ]
   },
