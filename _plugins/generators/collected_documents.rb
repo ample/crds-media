211fdbfabc8e22423635c54da3bd58c85368ab43
Warren Harrison <warren@hungry-media.com>
2019-03-18 16:44:37 -0400

    US16758 - Community Stories back-end
    Combining features and associated media while avoiding repeats.

--- _plugins/generators/collected_documents.rb
+++ _plugins/generators/collected_documents.rb
@@ -6,6 +6,18 @@ module Jekyll
       collectable_docs = site.documents.select{ |d| doc_types.include?(d.data['content_type']) }
       all_featured_media = site.collections['featured_media'].docs
       site.collections['collections'].docs.each do |collection|
+        # Get all docs that are featured for this collection, based on URL
+        permalink = "/collections/#{collection.data['slug']}"
+        features = all_featured_media.select { |m| m['page_path'] == collection.url}
+        featured_docs = []
+        if features.size > 0
+          # IDs of the entries attached to the feature in Contentful.
+          featured_doc_ids = features.first.data['entries'] ? features.first.data['entries'].collect { |e| e['id'] } : []
+          # Using the IDs, select the doc objects from the site and maintain the
+          # sort order of the doc_ids.
+          featured_docs = collectable_docs.select { |d| featured_doc_ids.include?(d.data['id']) }.sort_by { |d| featured_doc_ids.index(d.data['id']) }
+        end
+
         # Get all docs that have applied this collection.
         collected_docs = []
         collectable_docs.each do |collectable_doc|
@@ -14,28 +26,17 @@ module Jekyll
         end
         # Create a reference to publishable docs within this collection.
         # Docs that have a `published_at` field are considered publishable.
-        collection_media = collected_docs.select { |d| d.data['published_at'] }
+        collected_docs.select! { |d| d.data['published_at'] }
         # Publishable docs should be sorted in reverse chronological order.
-        collection_media.sort_by! { |d| d.data['published_at'] }.reverse!
-
-
-
-        # Get all docs that are featured for this collection, based on URL
-        permalink = "/collections/#{collection.data['slug']}"
-        features = all_featured_media.select { |m| m['page_path'] == collection.url}
-        if features.size > 0
-          # IDs of the entries attached to the feature in Contentful.
-          doc_ids = features.first.data['entries'] ? features.first.data['entries'].collect { |e| e['id'] } : []
-          # Using the IDs, select the doc objects from the site and maintain the
-          # sort order of the doc_ids.
-          featured_docs = collectable_docs.select { |d| doc_ids.include?(d.data['id']) }.sort_by { |d| doc_ids.index(d.data['id']) }
-        end
-
-        # TODO: if less than 7 featured items, shift from collected_media
-        # ensuring no repeats
+        collected_docs.sort_by! { |d| d.data['published_at'] }.reverse!
+        # Remove docs already featured
+        collected_docs.reject! { |d| featured_doc_ids.include?(d['id']) }
+        # If less than 7 featured items, shift from collected_docs
+        featured_docs << collected_docs.shift(7 - featured_docs.size)
+        featured_docs.flatten!
 
         collection.data['featured_media'] = featured_docs
-        collection.data['collection_media'] = collection_media
+        collection.data['collection_media'] = collected_docs
       end
     end
   end
